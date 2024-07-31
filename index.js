945d0dca19861665d45c545caf425657b371608a
leMinhPhuong <phuongleminh841@gmail.com>
2024-07-31 12:29:00 +0700

    add first script.

---
+++ index.js
@@ -0,0 +1,6 @@
+let secondPart = document.querySelector("summary entry-summary");
+let firstPart = document.querySelector(
+  "#product-5384 > div.woocommerce-product-gallery.woocommerce-product-gallery--with-images.woocommerce-product-gallery--columns-4.images > div > div > a > img"
+);
+
+console.log(firstPart, secondPart);
